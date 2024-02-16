b665be6848b73e9437cf3e0c36bf557c2afa93f1
Raighne <raighne.weng@gmail.com>
2024-02-16 14:40:30 +0800

    Release

--- src/components/Index.vue
+++ src/components/Index.vue
@@ -43,6 +43,13 @@
             </span>
           </li>
           <li>
+            <label class="left-label" contenteditable="true">{{$t('major')}}</label>
+            <span class="label-value" contenteditable="true">{{$t('myMajor')}}</span>
+            <span class="item-remove" style="visibility: hidden;">
+              <i class="iconfont icon-delete"></i>
+            </span>
+          </li>
+          <li>
             <label class="left-label" contenteditable="true">{{$t('graduateTime')}}</label>
             <span class="label-value" contenteditable="true">{{$t('myGraduateTime')}}</span>
             <span class="item-remove" style="visibility: hidden;">
@@ -67,6 +74,13 @@
           <li>
             <label class="left-label" contenteditable="true">{{$t('mobile')}}</label>
             <span class="label-value" contenteditable="true">{{$t('myMobile')}}</span>
+            <span class="item-remove" style="visibility: hidden;">
+              <i class="iconfont icon-delete"></i>
+            </span>
+          </li>
+          <li>
+            <label class="left-label" contenteditable="true">{{$t('mobile')}}</label>
+            <span class="label-value" contenteditable="true">{{$t('myMobile2')}}</span>
             <span class="item-remove" style="visibility: hidden;">
               <i class="iconfont icon-delete"></i>
             </span>
@@ -112,7 +126,7 @@
         <hr />
         <ul class="progress-list">
           <li>
-            <label class="left-label" contenteditable="true">Javascript</label>
+            <label class="left-label" contenteditable="true">TypeScript</label>
             <progress value="80" max="100"></progress>
             <span class="item-remove" style="visibility: hidden;">
               <i class="iconfont icon-delete"></i>
@@ -120,28 +134,14 @@
           </li>
           <li>
             <label class="left-label" contenteditable="true">Python</label>
-            <progress value="60" max="100"></progress>
-            <span class="item-remove" style="visibility: hidden;">
-              <i class="iconfont icon-delete"></i>
-            </span>
-          </li>
-          <li>
-            <label class="left-label" contenteditable="true">HTML/CSS</label>
             <progress value="70" max="100"></progress>
             <span class="item-remove" style="visibility: hidden;">
               <i class="iconfont icon-delete"></i>
             </span>
           </li>
           <li>
-            <label class="left-label" contenteditable="true">Android</label>
-            <progress value="40" max="100"></progress>
-            <span class="item-remove" style="visibility: hidden;">
-              <i class="iconfont icon-delete"></i>
-            </span>
-          </li>
-          <li>
-            <label class="left-label" contenteditable="true">C</label>
-            <progress value="43.90625" max="100"></progress>
+            <label class="left-label" contenteditable="true">Kubernetes</label>
+            <progress value="70" max="100"></progress>
             <span class="item-remove" style="visibility: hidden;">
               <i class="iconfont icon-delete"></i>
             </span>
@@ -162,8 +162,8 @@
         <hr />
         <ul class="stack-list">
           <li>
-            <label class="left-label" contenteditable="true">{{$t('frontEnd')}}</label>
-            <span class="label-value" contenteditable="true">jQuery、Vue.js</span>
+            <label class="left-label" contenteditable="true">{{$t('devOps')}}</label>
+            <span class="label-value" contenteditable="true">Kubernetes, Helm, Kong, Docker, CI/CD</span>
             <span class="item-remove" style="visibility: hidden;">
               <i class="iconfont icon-delete"></i>
             </span>
@@ -174,7 +174,7 @@
             <span
               class="label-value"
               contenteditable="true"
-            >Node.js TypeScript Python MongoDB MySQL Redis Elasticsearch SQS</span>
+            >Node.js TypeScript Python Go Lua MongoDB MySQL Redis Elasticsearch SQS</span>
             <span class="item-remove" style="visibility: hidden;">
               <i class="iconfont icon-delete"></i>
             </span>
@@ -191,7 +191,7 @@
       </div>
     </div>
     <div class="main">
-      <div class="education info-unit right-list">
+      <!-- <div class="education info-unit right-list">
         <h2 class="info-header">
           <i class="iconfont icon-education"></i>
           <span class="info-title" contenteditable="true">{{$t('education')}}</span>
@@ -212,7 +212,7 @@
             </span>
           </li>
         </ul>
-      </div>
+      </div> -->
       <div class="work info-unit right-list">
         <h2 class="info-header">
           <i class="iconfont icon-work"></i>
@@ -260,7 +260,7 @@
       <div class="aboutme info-unit right-paragraph">
         <h2 class="info-header">
           <i class="iconfont icon-flower"></i>
-          <span class="info-title" contenteditable="true">{{$t('selfAssement')}}</span>
+          <span class="info-title" contenteditable="true">{{$t('selfAssessment')}}</span>
           <span class="unit-remove" style="visibility: hidden;">
             <i class="iconfont icon-delete"></i>
           </span>
@@ -269,10 +269,7 @@
         <p contenteditable="true"></p>
         <p
           style="padding: 0px; cursor: default; color: rgb(0, 77, 64); font-family: 'Helvetica Neue', Helvetica, 'Lucida Grande', Arial, 'Hiragino Sans GB', 微软雅黑, 'WenQuanYi Micro Hei', STHeiti, SimSun, sans-serif; font-size: 18px; text-indent: 36px;"
-        >{{$t('selfAssement1')}}</p>
-        <p
-          style="padding: 0px; cursor: default; color: rgb(0, 77, 64); font-family: 'Helvetica Neue', Helvetica, 'Lucida Grande', Arial, 'Hiragino Sans GB', 微软雅黑, 'WenQuanYi Micro Hei', STHeiti, SimSun, sans-serif; font-size: 18px; text-indent: 36px;"
-        >{{$t('selfAssement2')}}</p>
+        >{{$t('mySelfAssessment')}}</p>
       </div>
     </div>
   </div>
